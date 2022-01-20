ea8fd982a71a0467aa95b76cb9a2d2bb01bcbc4a
Yatham Tejasri <94678025+Sridha123@users.noreply.github.com>
2022-01-20 16:28:26 +0000

    p5revised

--- p5original.c
+++ p5original.c
@@ -1,1 +1,28 @@
 #include<stdio.h>
+void input(int*a,int*b,int*c)
+{
+  printf("Enter three numbers\n");
+  scanf("%d %d %d",a,b,c);
+}
+void cmp(int a,int b,int c,int *l)
+{
+  if((a>b)&&(a>c))
+  printf("A is *big\n");
+  else if((b>a)&&(b>c))
+  printf("B is *big\n");
+  else if((c>a)&&(c>b))
+  printf("c is *big\n");
+}
+void output(int*l)
+{
+  printf("big number is:%d\n",*l);
+}
+int main()
+{
+  int a,b,c,l;
+  input(&a,&b,&c);
+  cmp(a,b,c,&l);
+  return 0;
+}
+
+
